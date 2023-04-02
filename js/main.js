95cc721acf93e2bdaadb9ee8d75643f1e6fdbee5
jodiecouch <couchjodie@gmail.com>
2023-04-01 21:58:45 -0600

    seperate js files for sep pages for now

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 import {clearFilters, filterResults, loadAds} from "./Trucks.mjs";
 
+
  const truck = loadAds();
- //console.log("Hey girl");
-
- //document.getElementById("model2500").onclick = filterResults(2500);
- // or
- /*
-let button = document.getElementById("2500");
-button.addEventListener("click",filterResults,false);
-*/
-
 var buttons = document.querySelectorAll("button.filter");
 for(var i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click",filterResults,false);
@@ -17,27 +9,4 @@ for(var i = 0; i < buttons.length; i++){
 
 let clear = document.getElementById("clearFilter");
 clear.addEventListener("click",clearFilters,false);
-
-/*
- const dataPlace = "data/trucks.json";
-  fetch(dataPlace)
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            const ads = json['ads'];
-            ads.forEach(displayData);
-            console.log("did I make it");
-        })
-        
- 
-    function displayData(ad){
-        console.log(`${ad.id}, ${ad.year}`);
-        }
-
-*/
-
-
-
-    
-
 
